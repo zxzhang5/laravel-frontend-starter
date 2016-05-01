1f5866709148d16ae710fc79935460df6b9fb742
howdy <zxzhang5@mail.ustc.edu.cn>
2016-05-01 10:11:51 +0800

    登录数据库

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -18,15 +18,21 @@ class UserTableSeeder extends Seeder
             [                
                 'name' => '管理员',
                 'mobile' => '13412345678',
-                'email' => 'test@qq.com',
-                'password' => 'test'
+                'email' => 'admin@qq.com',
+                'password' => '1234'
+            ],
+            [                
+                'name' => '普通用户',
+                'mobile' => '13412345679',
+                'email' => 'user@qq.com',
+                'password' => '1234'
             ]
         ];
 
         foreach ($users as $i => $user) {
             Sentinel::register($user, true);
             $u = User::where('name', '=', $user['name'])->first();
-            $u->roles()->sync([1, 2]);
+            $u->roles()->sync([$u->id]);
         }
     }
 
